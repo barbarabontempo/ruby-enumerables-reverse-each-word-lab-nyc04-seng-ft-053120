d190403987730b73a6003bd39c81c086698cea93
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-12 14:44:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,9 @@ require 'pry'
 
 def reverse_each_word(str)
   new_str = []
-
   str.split.collect { |word| reversed_str << word.reverse}
 binding.pry
 puts "hi"
   end
-  reversed_str.join(' ')
+  new_str.join(' ')
 end
